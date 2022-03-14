68be0f7cec72254ffd52f701c20454dfb9a4717d
whitel <heriskent@gmail.com>
2022-03-14 22:12:32 +0800

    complete

--- main.cpp
+++ main.cpp
@@ -18,6 +18,21 @@ static void printArray(vector<int> nums) {
     }
 }
 
+int binary_search(vector<int> nums, int target) {
+    int left = 0, right = nums.size();
+    while(left < right) {
+        int mid = left + (left + right) / 2;
+        if(nums[mid] > target) {
+            right = mid;
+        } else if(nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
 int main() {
     Solution s;
     vector<int> a = {6, 4, 0, 7, 8, 5, 9, 1, 2, 3};
