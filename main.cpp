3de384b97641ac7555de4fa7ecdc828cd86be165
whitel <heriskent@gmail.com>
2022-03-14 22:19:55 +0800

    complete

--- main.cpp
+++ main.cpp
@@ -4,7 +4,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
+        bubble_sort(nums);
         return nums;
+    }
+
+    void bubble_sort(vector<int> &nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = 0; j < nums.size() - i - 1; j++) {
+                if(nums[j-1] > nums[j])
+                    swap(nums[j-1], nums[j]);
+            }
+        }
     }
 };
 
