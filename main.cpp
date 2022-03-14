8798c7b85bbda00aa9ddc3621a935123cebd37e6
whitel <heriskent@gmail.com>
2022-03-14 22:14:10 +0800

    complete

--- main.cpp
+++ main.cpp
@@ -4,7 +4,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size());
         return nums;
+    }
+
+    void quickSort(vector<int> &nums, int left, int right) {
+        if(left < right) {
+            int privotLoc = partition(nums, left, right);
+            quickSort(nums, left, privotLoc);
+            quickSort(nums, privotLoc + 1, right);
+        }
+    }
+
+    int partition(vector<int> &nums, int left, int right) {
+        int privotKey = nums[left];
+        while(left < right) {
+            while(left < right && nums[right] >= privotKey) right--;
+            swap(nums[left], nums[right]);
+            while(left < right && nums[left] <= privotKey) left++;
+            swap(nums[left], nums[right]);
+        }
+        return left;
     }
 };
 
